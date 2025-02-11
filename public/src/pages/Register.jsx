aa98dbbfbfcd53ab59b92ab6478aeceaff42ccd5
NitisriGirkala <nitisrigirkala@gmail.com>
2025-02-10 23:50:44 -0600

    APIRoutes added

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -120,58 +120,132 @@
 //   }
 // `;
 
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { registerRoute } from "../utils/APIRoutes";
 
 export default function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert("Form Submitted");
+  const navigate = useNavigate();
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
 
-  const handleChange = (event) => {};
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      }
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
+    }
+  };
 
   return (
-    <Container>
-      <FormWrapper>
-        <div className="brand">
-          <img src={Logo} alt="Logo" />
-          <h1>Snappy</h1>
-        </div>
-        <form onSubmit={handleSubmit}>
+    <>
+      <Container>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
           <input
             type="text"
-            placeholder="Enter your username"
+            placeholder="Username"
             name="username"
-            onChange={handleChange}
+            onChange={(e) => handleChange(e)}
           />
           <input
             type="email"
-            placeholder="Enter your email"
+            placeholder="Email"
             name="email"
-            onChange={handleChange}
+            onChange={(e) => handleChange(e)}
           />
           <input
             type="password"
-            placeholder="Enter your password"
+            placeholder="Password"
             name="password"
-            onChange={handleChange}
+            onChange={(e) => handleChange(e)}
           />
           <input
             type="password"
-            placeholder="Confirm your password"
+            placeholder="Confirm Password"
             name="confirmPassword"
-            onChange={handleChange}
+            onChange={(e) => handleChange(e)}
           />
-          <button type="submit">Sign Up</button>
-          <p>
-            Already have an account? <Link to="/login">Log in</Link>
-          </p>
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
         </form>
-      </FormWrapper>
-    </Container>
+      </Container>
+      <ToastContainer />
+    </>
   );
 }
 
